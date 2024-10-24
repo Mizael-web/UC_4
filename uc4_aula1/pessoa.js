5b8c8fd524b1d54d0aee27d194d8a669956e6099
Mizael-web <mizaelmendes@gmail.com>
2024-10-24 13:39:27 -0300

    add.atividade jogo

--- uc4_aula1/pessoa.js
+++ uc4_aula1/pessoa.js
@@ -18,11 +18,63 @@ class Pessoa {
        this.sexo = sexo,
        this.nacionalidade = nacionalidade,
        this.estadoCivil = estadoCivil
-
      }
-     
+     getNome(){
+        return this.nome;
+     }  
+     getRg(){
+        return this.rg;
+     }
+     getCpf(){
+        return this.cfp;
+     }
+     getIdade(){
+      return this.idade;
+   }
+   getSexo(){
+    return this.sexo;
+ }
+ getNacionalidade(){
+  return this.nacionalidade;
 }
+getEstadoCivil(){
+  return this.estadoCivil;
+}
+setNome(nome){
+    this.nome = nome;
+}
+ 
+  exibirDados(){
+    console.log (
+      `
+        Dados Pessoais 
+        Nome: ${this.nome}
+        RG: ${this.rg}
+        Cpf: ${this.cpf}
+        Idade: ${this.idade}
+        Sexo : ${this.sexo}
+        Nacionalidade: ${this.nacionalidade}
+        EstadoCivil: ${this.estadoCivil}
+        `
+  )
+  }
+
+}
+
     
 
-let pessoa01 = new Pessoa("Mizael", 383393983, 9988888000999, 34,"masculino","brasileiro", "casado");
+let pessoa01 = new Pessoa("Mizael", "383393983", "9988888000999", "34","masculino","brasileiro", "casado");
 console.log(pessoa01);
+console.log (pessoa01.nome);
+console.log(pessoa01.getNome());
+console.log(pessoa01.getRg());
+console.log(pessoa01.getCpf());
+console.log(pessoa01.getIdade());
+console.log(pessoa01.getSexo());
+console.log(pessoa01.getNacionalidade());
+console.log(pessoa01.getEstadoCivil());
+pessoa01.setNome("Miza");
+pessoa01.exibirDados();
+
+
+
